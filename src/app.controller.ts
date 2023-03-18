39e69a1e5e135db713ca9604647af45160fcb5d2
david <david@david.david>
2023-03-18 19:54:04 +0200

    feat: improvements

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiResponse({ status: HttpStatus.OK, description: 'OK' })
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,12 +1,25 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
 
 let server: Handler;
 
+function setupSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('Documentation for the API')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
+
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
